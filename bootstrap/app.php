e6b204e5e63985db876417db28f0021f683f44c1
Fian <fian.kurus06@gmail.com>
2026-08-02 13:45:30 +0700

    feat: enable trustProxies('*') and add ASSET_URL support for seamless Tailscale Funnel sub-path routing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         // Middleware untuk rute web
         $middleware->web(append: [
             \Illuminate\Session\Middleware\StartSession::class,
